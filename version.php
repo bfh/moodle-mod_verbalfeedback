96a646ba4bbfbdf1272adca50456ddbcd0ba43b1
Luca Bösch <luca.boesch@bfh.ch>
2022-07-05 17:45:35 +0200

    Version 1.0.1 (2022061301).

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_verbalfeedback';
-$plugin->release = '1.0.0';
+$plugin->release = '1.0.1';
 $plugin->version = 2022061301;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
