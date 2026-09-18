41b514eb12587f574be022d999c2443797001e65
Stephan Robotta <stephan.robotta@bfh.ch>
2026-09-18 11:56:27 +0200

    Clean webservice functions that apparently are not completely implemented.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_verbalfeedback';
-$plugin->version = 2026060500;
+$plugin->version = 2026060501;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron      = 0;
